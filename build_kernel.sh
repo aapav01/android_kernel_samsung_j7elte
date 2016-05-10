ae62a16b0094170526d04b0adb40f20e40e362ba
Apavayan <info@apavayan.com>
2016-05-10 17:58:54 +0530

    Add Kernel building Srcipt

---
+++ build_kernel.sh
@@ -0,0 +1,14 @@
+
+
+export ARCH=arm64
+export CROSS_COMPILE=~/cm13/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+mkdir -p output
+
+make -C $(pwd) O=output exynos7580-turbo-j7elte_defconfig
+
+mkdir -p output/init
+cp -vf init/vmm.elf output/init/vmm.elf
+
+make -C $(pwd) O=output -j10
+
+
