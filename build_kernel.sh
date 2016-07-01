c6960087ac47d5cda2ed9d8d172294b6d04e2da1
Apavayan Sinha <info@apavayan.com>
2016-07-01 14:45:31 +0530

    build_kernel: Switch the toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 
 
 export ARCH=arm64
-export CROSS_COMPILE=~/cm13/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/working/kernel/linaro-5.3-64/bin/aarch64-linux-android-
 mkdir -p output
 
 make -C $(pwd) O=output exynos7580-turbo-j7elte_defconfig
