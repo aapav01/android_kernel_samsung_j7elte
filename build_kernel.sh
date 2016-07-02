5ba7032949b202092bbb069a5c2f9a1340fff33b
Apavayan Sinha <info@apavayan.com>
2016-07-02 09:36:42 +0530

    build_kernel: Gcc changed to 4.9
    Signed-off-by: Apavayan Sinha <info@apavayan.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 
 
 export ARCH=arm64
-export CROSS_COMPILE=~/working/kernel/linaro-5.3-64/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/working/kernel/linaro-4.9-64/bin/aarch64-none-elf-
 mkdir -p output
 
 make -C $(pwd) O=output exynos7580-turbo-j7elte_defconfig
